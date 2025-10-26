7db77c46d4bfd93482ced94cd4ac41fc0575a656
KokomiCat <xiazeyu@users.noreply.github.com>
2025-10-26 12:20:32 -0400

    Reduce default max_count in _get_connection_count

--- src/libs/FastTelethon.py
+++ src/libs/FastTelethon.py
@@ -172,7 +172,7 @@ class ParallelTransferrer:
 
     @staticmethod
     def _get_connection_count(
-        file_size: int, max_count: int = 20, full_size: int = 100 * 1024 * 1024
+        file_size: int, max_count: int = 4, full_size: int = 100 * 1024 * 1024
     ) -> int:
         if file_size > full_size:
             return max_count
